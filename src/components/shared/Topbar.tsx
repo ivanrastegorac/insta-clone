0cc06660e6dc85553ca21de3c22dd0af57944aaa
irastegorac <ivan.rastegorac@ag04.com>
2023-12-30 09:51:15 +0100

    Add logo and signOut button

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../ui/button";
 
 const Topbar = () => {
-  return <div>Topbar</div>;
+  return (
+    <section className="topbar">
+      <div className="flex-between py-4 px-5">
+        <Link to="/" className="flex gap-3 items-center">
+          <img
+            src="/public/assets/images/logo.svg"
+            alt="logo"
+            width={130}
+            height={325}
+          />
+        </Link>
+
+        <div className="flex gap-4">
+          <Button
+            variant="ghost"
+            className="shad-button_ghost"
+            onClick={() => {}}
+          >
+            <img src="/public/assets/icons/logout.svg" alt="logout" />
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Topbar;
