d3cecee73a1498f32d6e4d31d8713ea99c540a90
irastegorac <ivan.rastegorac@ag04.com>
2024-01-06 10:46:11 +0100

    Add basic UI to LeftSidebar component

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { useUserContext } from "@/context/AuthContext";
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const LeftSidebar = () => {
-  return <div>LeftSidebar</div>;
+  const { isSuccess } = useSignOutAccount();
+  const navigate = useNavigate();
+  const user = useUserContext();
+
+  useEffect(() => {
+    if (isSuccess) {
+      navigate(0);
+    }
+  }, [isSuccess]);
+
+  return (
+    <nav className="leftsidebar">
+      <div className="flex flex-col gap-11">
+        <Link to="/" className="flex gap-3 items-center">
+          <img
+            src="/public/assets/images/logo.svg"
+            alt="logo"
+            width={170}
+            height={36}
+          />
+        </Link>
+        <Link
+          to={`/profile/${user.user.id}`}
+          className="flex gap-3 items-center"
+        >
+          <img
+            src={user.user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+            alt="profile"
+            className="h-14 w-14 rounded-full"
+          />
+          <div className="flex flex-col">
+            <p className="body-bold">{user.user.name}</p>
+            <p className="small-regular text-light-3">@{user.user.username}</p>
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default LeftSidebar;
