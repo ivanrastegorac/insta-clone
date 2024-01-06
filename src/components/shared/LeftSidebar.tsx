1728a2167cc43590b1b7ad0bb1e3cf1690e95208
irastegorac <ivan.rastegorac@ag04.com>
2024-01-06 11:05:34 +0100

    Add sidebar links and style it

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,10 +1,12 @@
+import { sidebarLinks } from "@/constants";
 import { useUserContext } from "@/context/AuthContext";
 import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
+import { INavLink } from "@/types";
 import { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 const LeftSidebar = () => {
-  const { isSuccess } = useSignOutAccount();
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
   const navigate = useNavigate();
   const user = useUserContext();
 
@@ -39,6 +41,25 @@ const LeftSidebar = () => {
             <p className="small-regular text-light-3">@{user.user.username}</p>
           </div>
         </Link>
+        <ul className="flex flex-col gap-6">
+          {sidebarLinks.map((link: INavLink) => {
+            return (
+              <li key={link.label} className="leftsidebar-link">
+                <NavLink
+                  to={link.route}
+                  className="flex gap-4 items-center p-4"
+                >
+                  <img
+                    src={link.imgURL}
+                    alt={link.label}
+                    className="group-hover:invert-white"
+                  />
+                  {link.label}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </nav>
   );
