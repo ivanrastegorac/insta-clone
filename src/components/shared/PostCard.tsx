b09f575412c915bda69a2dd5f41c5c474fee3a8d
ivanrastegorac <ivan.rastegorac@gmail.com>
2024-01-14 22:20:25 +0100

    Add a new section to display the post creator's name, creation timestamp, and location in the UI

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -12,10 +12,25 @@ const PostCard = ({post}: PostCardProps) => {
                 <Link to={`/profile/${post.creator.$id}`}>
                     <img src={post?.creator?.imageUrl || '/public/assets/icons/profile-placeholder.svg'} alt='creator' className="rounded-full w-12 lg:h-12"/>
                 </Link>
+
+                <div className="flex flex-col">
+                    <p className="base-medium lg:body-bold text-light-1">
+                        {post.creator.name}
+                    </p>
+                    <div className="flex-center gap-2 text-light-3">
+                        <p className="subtle-semibold lg:small-regular">
+                           {post.$createdAt} 
+                        </p>
+                        -
+                        <p className="subtle-semibold lg:small-regular">
+                            {post.location}
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
   )
 }
 
-export default PostCard
+export default PostCard;
